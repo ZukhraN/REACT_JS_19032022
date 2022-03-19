88da41beac888e52720177cd89d780e98f7e0185
Zukhra Nurmanova <zukhra.nurmanova@gmail.com>
2022-03-19 18:07:55 +0600

    190322

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header=()=>{
+    return(
+        <header className='header'>
+            <img src='http://buildicus.com/blog/img/uploads/2014/07/Yourlogo.jpg'/>
+        </header>  
+    )
+};
+
+export default Header;
---
+++ src/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile=()=>{
+    return(
+        <div className='content'>
+            <div>
+                <img src='https://thumbs.dreamstime.com/b/belle-photo-panoramique-d-aurore-bor%C3%A9ale-%C3%A0-travers-le-horizont-entier-sur-les-feux-de-for%C3%AAt-et-la-ville-pins-dans-campagne-202218275.jpg'/>
+            </div>
+            <div>
+                Ava+description
+            </div>      
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Profile;
